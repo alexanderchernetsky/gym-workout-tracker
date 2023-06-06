a484791fc0553f9ead1c2deb4ffa7bc6fb14016a
alex <a.chernetsky@itransition.com>
2023-06-06 13:43:45 +0100

    Run app on a web server before running e2e tests

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,23 +1,5 @@
 import {test, expect} from '@playwright/test';
 
-// todo: remove examples
-test('has title', async ({page}) => {
-    await page.goto('https://playwright.dev/');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({page}) => {
-    await page.goto('https://playwright.dev/');
-
-    // Click the get started link.
-    await page.getByRole('link', {name: 'Get started'}).click();
-
-    // Expects the URL to contain intro.
-    await expect(page).toHaveURL(/.*intro/);
-});
-
 test('login (happy path)', async ({page}) => {
     await page.goto('http://localhost:3000/');
     await page.getByLabel('Email *').fill('test@test.ru');
@@ -49,4 +31,18 @@ test('register (happy path)', async ({page}) => {
     await page.getByRole('button', {name: 'Register'}).click();
     await expect(page.getByRole('button', {name: 'Register'})).toBeDisabled();
     await expect(page).toHaveURL('http://localhost:3000/login');
+});
+
+test('register (validation)', async ({page}) => {
+    await page.goto('http://localhost:3000/register');
+    await page.getByLabel('Username *').click();
+    await page.getByLabel('Username *').fill('test user');
+    await page.getByLabel('Email *').click();
+    await page.getByLabel('Email *').fill('test@test');
+    await page.getByLabel('Password *').click();
+    await page.getByLabel('Password *').fill('123');
+    await page.getByRole('button', {name: 'Register'}).click();
+    await expect(page.getByText('Invalid email address')).toBeVisible();
+    await expect(page.getByText('Password must have at least 5 characters')).toBeVisible();
+    await expect(page.getByText('Username can only contain alphanumeric characters, dots, underscores')).toBeVisible();
 });
